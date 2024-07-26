9d99ae101451959c8f185412320ad2d7b28d4186
lyz4r <aid19.02.06@mail.ru>
2024-07-26 23:05:08 +0300

    добавил скрипт с т.н. блюром (наложение прозрачности)

---
+++ scripts/blur.js
@@ -0,0 +1,17 @@
+function addBlur(button){
+    button.addEventListener("mouseover", ()=>{
+        event.target.classList.add("blur");
+    })
+    button.addEventListener("mouseout", ()=>{
+        event.target.classList.remove("blur");
+    })
+}
+
+document.querySelectorAll(".btnw").forEach(button => addBlur(button));
+document.querySelectorAll(".btng").forEach(button => addBlur(button));
+document.querySelectorAll(".joinTeam").forEach(button => addBlur(button));
+document.querySelectorAll(".logo").forEach(button => addBlur(button));
+if (window.location.pathname == "/socom-website/index.html"){
+    console.log("fayno");
+    document.querySelectorAll(".socNetworks").forEach(button => addBlur(button));
+}
