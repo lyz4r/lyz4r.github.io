d10c5cc14c12443fffc11c1709a64f4c3604b99c
lyz4r <aid19.02.06@mail.ru>
2024-08-02 20:09:48 +0300

    временный фикс

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,17 @@ const ds = document.querySelectorAll(".socNetworks")[1];
 ds.addEventListener("click", () => {
     window.location.href = "https://discord.gg/2jUJFmaJmu";
 })
+
+document.querySelectorAll(".socNetworks").forEach(button => addBlur(button));
+document.querySelectorAll(".joinTeam").forEach(button => addBlur(button));
+document.querySelectorAll(".joinTeam").forEach(button => {
+    button.addEventListener("mouseover", () => {
+        button.innerText = `Стать\nгомосексуалистом`;
+        document.querySelector(".astolfo").classList.toggle("hidden");
+    });
+    button.addEventListener("mouseout", () => {
+        button.innerText = `Присоединиться\nк SOCOM AIRSOFT`;
+        document.querySelector(".astolfo").classList.toggle("hidden");
+    }
+    );
+});
