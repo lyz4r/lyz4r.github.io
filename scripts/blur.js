4f85f03c7e5f3a2be9f2ded0ed1aa1c2093f6cef
lyz4r <aid19.02.06@mail.ru>
2024-08-02 20:43:32 +0300

    i hate it

--- scripts/blur.js
+++ scripts/blur.js
@@ -7,14 +7,27 @@ function addBlur(button){
     })
 }
 
-export { addBlur };
-
 document.querySelectorAll(".btnw").forEach(button => addBlur(button));
 document.querySelectorAll(".btng").forEach(button => addBlur(button));
 document.querySelectorAll(".logo").forEach(button => addBlur(button));
+if (window.location.pathname == "/index.html"){
+    console.log("main page was recognised");
+    document.querySelectorAll(".socNetworks").forEach(button => addBlur(button));
+    document.querySelectorAll(".joinTeam").forEach(button => addBlur(button));
+    document.querySelectorAll(".joinTeam").forEach(button => {
+        button.addEventListener("mouseover", ()=> {
+            button.innerText = `Стать\nгомосексуалистом`;
+            document.querySelector(".astolfo").classList.toggle("hidden");
+        });
+        button.addEventListener("mouseout", () => {
+            button.innerText = `Присоединиться\nк SOCOM AIRSOFT`;
+            document.querySelector(".astolfo").classList.toggle("hidden");
+        }
+        );
+    });
+}
 
-
-if (window.location.pathname == "/socom-website/questionary.html"){
+if (window.location.pathname == "/questionary.html"){
     console.log("quiz was recognised");
     addBlur(document.querySelector(".submit"));
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { addBlur } from "./blur";
+
 
 const vk = document.querySelector(".socNetworks");
 vk.addEventListener("click", () => {
@@ -8,17 +8,3 @@ const ds = document.querySelectorAll(".socNetworks")[1];
 ds.addEventListener("click", () => {
     window.location.href = "https://discord.gg/2jUJFmaJmu";
 })
-
-document.querySelectorAll(".socNetworks").forEach(button => addBlur(button));
-document.querySelectorAll(".joinTeam").forEach(button => addBlur(button));
-document.querySelectorAll(".joinTeam").forEach(button => {
-    button.addEventListener("mouseover", () => {
-        button.innerText = `Стать\nгомосексуалистом`;
-        document.querySelector(".astolfo").classList.toggle("hidden");
-    });
-    button.addEventListener("mouseout", () => {
-        button.innerText = `Присоединиться\nк SOCOM AIRSOFT`;
-        document.querySelector(".astolfo").classList.toggle("hidden");
-    }
-    );
-});
