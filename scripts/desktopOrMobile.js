40594b3deecca1d455333101f1829ed873ccea83
lyz4r <aid19.02.06@mail.ru>
2024-08-05 11:22:49 +0300

    пожалуйста, заработай

--- scripts/desktopOrMobile.js
+++ scripts/desktopOrMobile.js
@@ -1,15 +1,15 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-    document.write("mobile device");
+    console.log("mobile device");
     const styleMain = document.querySelector(".styleIndex");
     const styleKitList = document.querySelector(".styleKitList");
     const styleQuiz = document.querySelector(".styleQuiz");
     styleMain.innerHTML = `<link class="styleIndex" rel="stylesheet" href="./styleMobile.css"></link>`;
-    styleKitList.innerHTML = `<link class="styleIndex" rel="stylesheet" href="./styleKitListMobile.css"></link>`;
-    styleQuiz.innerHTML = `<link class="styleIndex" rel="stylesheet" href="./styleQuizMobile.css"></link>`;
+    styleKitList.innerHTML = `<link class="styleKitList" rel="stylesheet" href="./styleKitListMobile.css"></link>`;
+    styleQuiz.innerHTML = `<link class="styleQuiz" rel="stylesheet" href="./styleQuizMobile.css"></link>`;
   }else{
     // false for not mobile device
-    document.write("not mobile device");
+    console.log("not mobile device");
   }
 
 
