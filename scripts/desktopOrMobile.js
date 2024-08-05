c51d9ae390f8f5b05269a9b737429c3e0db01370
lyz4r <aid19.02.06@mail.ru>
2024-08-05 11:35:55 +0300

    я тебе благаю

--- scripts/desktopOrMobile.js
+++ scripts/desktopOrMobile.js
@@ -1,15 +1,23 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     // true for mobile device
     console.log("mobile device");
-    const styleMain = document.querySelector(".styleIndex");
-    const styleKitList = document.querySelector(".styleKitList");
-    const styleQuiz = document.querySelector(".styleQuiz");
-    styleMain.innerHTML = `<link class="styleIndex" rel="stylesheet" href="./styleMobile.css"></link>`;
-    styleKitList.innerHTML = `<link class="styleKitList" rel="stylesheet" href="./styleKitListMobile.css"></link>`;
-    styleQuiz.innerHTML = `<link class="styleQuiz" rel="stylesheet" href="./styleQuizMobile.css"></link>`;
-  }else{
+    
+    // Create link elements for mobile styles
+    const mobileStyleLink1 = document.createElement('link');
+    mobileStyleLink1.rel = 'stylesheet';
+    mobileStyleLink1.href = './styleMobile.css';
+    document.head.appendChild(mobileStyleLink1);
+
+    const mobileStyleLink2 = document.createElement('link');
+    mobileStyleLink2.rel = 'stylesheet';
+    mobileStyleLink2.href = './styleKitListMobile.css';
+    document.head.appendChild(mobileStyleLink2);
+
+    const mobileStyleLink3 = document.createElement('link');
+    mobileStyleLink3.rel = 'stylesheet';
+    mobileStyleLink3.href = './styleQuizMobile.css';
+    document.head.appendChild(mobileStyleLink3);
+} else {
     // false for not mobile device
     console.log("not mobile device");
-  }
-
-
+}
