7465fd2af1463f9a805c68a3dac4a4f5ccac0240
lyz4r <aid19.02.06@mail.ru>
2024-08-05 12:43:15 +0300

    Боже праведный, пусть оно заработает

--- scripts/desktopOrMobile.js
+++ scripts/desktopOrMobile.js
@@ -5,20 +5,20 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     // для мобилки
     const mobileStyleLink1 = document.createElement('link');
     mobileStyleLink1.rel = 'stylesheet';
-    mobileStyleLink1.href = './styleMobile.css';
+    mobileStyleLink1.href = './styles/styleMobile.css';
     document.head.appendChild(mobileStyleLink1);
 
     if (window.location.pathname == "/kit-list.html") {
         const mobileStyleLink2 = document.createElement('link');
         mobileStyleLink2.rel = 'stylesheet';
-        mobileStyleLink2.href = './styleKitListMobile.css';
+        mobileStyleLink2.href = './styles/styleKitListMobile.css';
         document.head.appendChild(mobileStyleLink2);
     }
 
     if (window.location.pathname == "/questionary.html") {
         const mobileStyleLink3 = document.createElement('link');
         mobileStyleLink3.rel = 'stylesheet';
-        mobileStyleLink3.href = './styleQuizMobile.css';
+        mobileStyleLink3.href = './styles/styleQuizMobile.css';
         document.head.appendChild(mobileStyleLink3);
     }
 
@@ -29,20 +29,20 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     // для десктопа
     const styleLink1 = document.createElement('link');
     styleLink1.rel = 'stylesheet';
-    styleLink1.href = './style.css';
+    styleLink1.href = './styles/style.css';
     document.head.appendChild(styleLink1);
 
     if (window.location.pathname == "/kit-list.html") {
         const styleLink2 = document.createElement('link');
         styleLink2.rel = 'stylesheet';
-        styleLink2.href = './styleKitList.css';
+        styleLink2.href = './styles/styleKitList.css';
         document.head.appendChild(mobileStyleLink2);
     }
 
     if (window.location.pathname == "/questionary.html") {
         const styleLink3 = document.createElement('link');
         styleLink3.rel = 'stylesheet';
-        styleLink3.href = './styleQuiz.css';
+        styleLink3.href = './styles/styleQuiz.css';
         document.head.appendChild(styleLink3);
     }
 }
