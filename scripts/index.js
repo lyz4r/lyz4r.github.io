8b3feaae5d6401b3081e74bbfb6f80dfe50a1b0a
lyz4r <aid19.02.06@mail.ru>
2024-09-26 22:22:59 +0300

    готовая внешняя часть для десктопа

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,22 @@ ds.addEventListener("click", () => {
 })
 
 
+console.log("main page was recognised");
+
+document.querySelectorAll(".joinTeam").forEach(button => {
+    button.addEventListener("mouseover", ()=> {
+        button.innerText = `Welcome to the\nclub, buddy`;
+        console.log("Somebody wants to join our sect... God save his soul")
+        document.querySelector(".astolfo").classList.toggle("hidden");
+    });
+    button.addEventListener("mouseout", () => {
+        button.innerText = `Присоединиться\nк SOCOM AIRSOFT`;
+        document.querySelector(".astolfo").classList.toggle("hidden");
+    }
+    );
+});
+
+
 let flag = 0;
 var delayedShow;
 
---
+++ scripts/quetionaryChecker.js
@@ -0,0 +1,13 @@
+const nickname = document.querySelector(".nickname");
+const phone = document.querySelector(".phone")
+const textarea = document.querySelector("textarea");
+const vkref = document.querySelector(".vkref");
+const occupation = document.querySelector(".occupation");
+const send = document.querySelector(".send");
+
+
+send.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const formData = new FormData(formElem);
+    
+})
