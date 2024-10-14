be44173f0714b0322674360aec4902ae8bb76d42
lyz4r <aid19.02.06@mail.ru>
2024-10-14 19:10:58 +0300

    работает форма подачи заявок

--- scripts/quetionaryChecker.js
+++ scripts/quetionaryChecker.js
@@ -1,13 +1,48 @@
-const nickname = document.querySelector(".nickname");
-const phone = document.querySelector(".phone")
-const textarea = document.querySelector("textarea");
-const vkref = document.querySelector(".vkref");
 const occupation = document.querySelector(".occupation");
-const send = document.querySelector(".send");
+const occupationSel = document.getElementById("occupation");
 
-
-send.addEventListener("click", (e)=>{
-    e.preventDefault();
-    const formData = new FormData(formElem);
-    
+occupationSel.addEventListener("change", ()=>{
+    if (occupationSel.value == "other"){
+        occupation.classList.remove("excluded");
+    }
+    else if (occupationSel.value != "other"){
+        occupation.classList.add("excluded");
+    }
 })
+
+document.getElementById("formElem").addEventListener("submit", function (e) {
+    e.preventDefault();
+  
+    let formData = new FormData(formElem);
+
+    console.log(Object.fromEntries(formData));
+
+    emailjs.init({
+        publicKey: 'BLpflIvrecPuO8xmP',
+        limitRate: {
+          // Set the limit rate for the application
+          id: 'app',
+          // Allow 1 request per 10s
+          throttle: 10000,
+        },
+      });
+
+
+        emailjs.send('service_miduali', 'template_lzle293', Object.fromEntries(formData)).then(
+            (response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            },
+            (error) => {
+              console.log('FAILED...', error);
+            },
+          );
+     });
+
+    /*function sendEmail() {
+        Email.send(formData)
+            .then(function () {
+                alert("mail sent successfully")
+            });
+    }
+  });*/
+
