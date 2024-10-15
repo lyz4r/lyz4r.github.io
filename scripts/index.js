fa3c055b7eb74cf33ff499f99e9a7100d4c5db5a
lyz4r <aid19.02.06@mail.ru>
2024-10-15 14:09:13 +0300

    mobile version hurray!

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,6 @@ const vk = document.querySelector(".socNetworks");
 vk.addEventListener("click", () => {
     window.location.href = "https://vk.com/socom_airsoft_team";
 })
-const ds = document.querySelectorAll(".socNetworks")[1];
-ds.addEventListener("click", () => {
-    window.location.href = "https://discord.gg/2jUJFmaJmu";
-})
 
 
 console.log("main page was recognised");
